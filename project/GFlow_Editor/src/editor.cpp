930876b8ec0e5cefc2148af92da61bb856d21ce5
AsperTheDog <guillerman0000@gmail.com>
2024-04-21 16:55:30 +0200

    Removed multi-window support due to many problems

--- project/GFlow_Editor/src/editor.cpp
+++ project/GFlow_Editor/src/editor.cpp
@@ -88,7 +88,7 @@ void Editor::initImgui()
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
 	ImGui::StyleColorsDark();
 	ImGuiStyle& style = ImGui::GetStyle();
